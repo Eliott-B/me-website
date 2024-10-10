3358f60304553703367f8a932e0d43441ceea306
Eliott-B <eliottb.info@gmail.com>
2024-10-10 19:19:26 +0200

    :rocket: | Deploy a part of the home page

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Globalization;
 using Portfolio;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +8,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+var culture = new CultureInfo("fr-FR");
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
 
 await builder.Build().RunAsync();
