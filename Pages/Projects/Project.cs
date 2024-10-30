05b37a3dcc346322e676f6992a0a69cff76fe943
Eliott-B <eliottb.info@gmail.com>
2024-10-30 21:28:03 +0100

    :art: | Move LoadProjects function to C# file

--- Pages/Projects/Project.cs
+++ Pages/Projects/Project.cs
@@ -1,3 +1,5 @@
+using System.Xml;
+
 namespace Portfolio.Project
 {
     public enum ProjectType
@@ -10,13 +12,13 @@ namespace Portfolio.Project
 
     public class Project
     {
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Image { get; set; }
-        public string Link { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+        public string? Link { get; set; }
         public ProjectType Type { get; set; }
-        public string StartDate { get; set; }
-        public string EndDate { get; set; }
+        public string? StartDate { get; set; }
+        public string? EndDate { get; set; }
 
         public static string ProjectTypeToString(ProjectType type)
         {
@@ -38,6 +40,56 @@ namespace Portfolio.Project
                 "Professionel" => ProjectType.Professional,
                 _ => ProjectType.Unknown
             };
+        }
+
+        public static List<Project> LoadProjects(string xmlContent)
+        {
+            List<Project> projects = new List<Project>();
+
+            try
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(xmlContent);
+                XmlNodeList projectsList = xmlDoc.GetElementsByTagName("project");
+                if (projectsList.Count == 0)
+                {
+                    throw new FileNotFoundException("Aucun projet trouvé dans le fichier");
+                }
+                foreach (XmlNode project in projectsList)
+                {
+                    XmlNode? title = project.SelectSingleNode("name");
+                    XmlNode? image = project.SelectSingleNode("image");
+                    XmlNode? description = project.SelectSingleNode("description");
+                    XmlNode? page = project.SelectSingleNode("page");
+                    XmlNode? type = project.SelectSingleNode("type");
+                    XmlNode? startDate = project.SelectSingleNode("date-start");
+                    XmlNode? endDate = project.SelectSingleNode("date-end");
+                    if (title == null || image == null || description == null || page == null || type == null || startDate == null || endDate == null)
+                    {
+                        throw new XmlException("Erreur de formatage du fichier XML");
+                    }
+                    projects.Add(new Project
+                    {
+                        Title = title.InnerText,
+                        Image = image.InnerText,
+                        Description = description.InnerText,
+                        Link = page.InnerText,
+                        Type = StringToProjectType(type.InnerText),
+                        StartDate = startDate.InnerText,
+                        EndDate = endDate.InnerText
+                    });
+                }
+            }
+            catch (XmlException ex)
+            {
+                throw new Exception("Erreur lors du chargement des projets : erreur de formatage du fichier XML", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erreur lors du chargement des projets", ex);
+            }
+
+            return projects;
         }
     }
 }
